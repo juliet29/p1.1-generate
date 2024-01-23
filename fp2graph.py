4189177a35522e95bd1d50c0e19c582b9b03abc5
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2024-01-22 17:00:49 -0800

    adjacency work

--- fp2graph.py
+++ fp2graph.py
@@ -4,6 +4,10 @@ import pandas as pd
 
 import imageio.v3 as iio
 
+from matplotlib.colors import Normalize, LogNorm
+
+from sklearn.cluster import KMeans
+
 import skimage.segmentation as seg
 from skimage.segmentation import mark_boundaries
 import skimage.measure as meas
@@ -13,48 +17,7 @@ import shapely.plotting as splt
 
 import networkx as nx
 
-RGB_CHANNEL_SIZE = 3
-
-COLORWAY = ['#702632', '#A4B494', '#495867',  "#81909E", "#F4442E", "#DB7C26", "#BB9BB0"]
-
-GM = (np.sqrt(5)-1.0)/2.0
-W = 8
-H = W*GM
-SIZE = (W, H)
-
-class Bbox():
-    def __init__(self, arr):
-        assert len(arr) == 4
-        self.min_row=arr[0]
-        self.min_col=arr[1]
-        self.max_row=arr[2]
-        self.max_col=arr[3]
-
-class Region():
-    bbox:list = None
-    coords:list = None
-    shape:Polygon = None
-    centroid:tuple = None
-
-
-def create_coords(b:Bbox): 
-    # clockwise from origin in bottom-left corner to match example: 
-    # https://shapely.readthedocs.io/en/stable/reference/shapely.LinearRing.html#shapely.LinearRing
-    coords = [
-        # col ==> x, row ==> y 
-        (b.min_col, b.min_row),
-        (b.max_col, b.min_row),
-        (b.max_col, b.max_row),
-        (b.min_col, b.max_row),
-    ]
-    return coords
-
-
-def check_adjacency(curr_region:Region.shape, nb_region:Region.shape):
-    # shapely check for adjacency 
-    check = curr_region.touches(nb_region)
-    return int(check) # True = 1, False = 0
-
+from helpers import *
 
 
 
@@ -87,20 +50,57 @@ class FloorPlan2Graph:
 
         self.tensor_labels = np.array(self.df["Label"]).reshape(self.tensor_shape[:2])
         return 
+    
+    def further_segment_doors(self, num_doors=6):
+        target_values = np.array([DOOR_CODE]*3)
+        check = np.all(self.tensor == target_values, axis=-1)
+        self.indices_split = np.where(check)
+        self.indices = np.argwhere(check)
+
+        # cluster and re-label doors so they have their own regions individual 
+        np.random.seed(42)
+        kmeans = KMeans(n_clusters=num_doors, n_init=10, init="k-means++") # TODO figure out why 
+        y_pred = kmeans.fit_predict(self.indices)
+
+        self.tensor_labels_w_doors = self.tensor_labels.copy()
+        self.tensor_labels_w_doors = self.tensor_labels_w_doors.astype(np.int64)
+
+        for x, y, door_label in zip(self.indices_split[0], self.indices_split[1], y_pred):
+            self.tensor_labels_w_doors[x,y] = DOOR_CODE+door_label 
+
 
 
     def array2shapely(self):
         assert type(self.tensor_labels) != None
-        self.all_region_props = meas.regionprops(self.tensor_labels, self.tensor)
+        self.all_region_props = meas.regionprops(self.tensor_labels_w_doors, self.tensor)
 
         self.regions = []
         for ix, region in enumerate(self.all_region_props): 
             r = Region()
+
+            # keep track of the colors in each region 
+            int0 = region.image_intensity
+            s = int0.shape
+            int01 = np.reshape(int0, (s[0] * s[1], 3))
+            unique_tuples = {tuple(arr) for arr in int01}
+            r.unique_colors = unique_tuples
+
             r.bbox = region.bbox
             r.coords = create_coords(Bbox(r.bbox))
             r.shape = Polygon(r.coords)
             r.centroid = (r.shape.centroid.x, r.shape.centroid.y)
-            self.regions.append(r)
+            r.label = region.label
+            r.area = region.bbox_area
+
+            # TODO code this as doors
+            if r.unique_colors == {(177, 177, 177)}:
+                r.type = RegionType.DOOR
+            else:
+                r.type = RegionType.ROOM
+
+            # ignore all regions that are not real rooms 
+            if r.unique_colors != {(0,0,0)}:
+                self.regions.append(r)
         return 
 
             
@@ -121,14 +121,16 @@ class FloorPlan2Graph:
         n_nodes = len(self.regions) 
         self.CG = nx.complete_graph(n_nodes)  
 
-        # set edge properties -> 0 = all unconnected to begin 
-        init_adj = np.zeros((n_nodes, n_nodes))
+        region_data = [{"type": getattr(instance, "type", None), "label": getattr(instance, "label", None)} for instance in self.regions]
+
+        node_attrs = dict(zip(list(self.CG.nodes), region_data))
+        nx.set_node_attributes(self.CG, node_attrs)
 
         # need to have labels / indices for the edge properties that explicitly index into the matrix 
         n = np.linspace(0, n_nodes-1, n_nodes)
         X2D,Y2D = np.meshgrid(n,n)
-        attrs = {(x,y): {"adj": i} for x,y,i in 
-                zip(Y2D.ravel(), X2D.ravel(), init_adj.ravel())}
+        attrs = {(x,y): {"adj": 0} for x,y,in 
+                zip(Y2D.ravel(), X2D.ravel())}
 
         nx.set_edge_attributes(self.CG, attrs)
         return 
@@ -162,19 +164,32 @@ class FloorPlan2Graph:
 
     def view_plan_segments(self):
         assert type(self.tensor_labels) != None
+
         border_color = [0,1,1]
-        plt.imshow(mark_boundaries(self.tensor, self.tensor_labels,color=border_color))
-        return plt 
+        label_nums = np.unique(self.tensor_labels_w_doors)
+        
+        norm = LogNorm(vmin=np.min(label_nums), vmax=np.max(label_nums))
+        cmap = plt.get_cmap("turbo")
+
+        fig, ax = plt.subplots()
+        im = ax.imshow(mark_boundaries(self.tensor, self.tensor_labels,color=border_color))
+
+        fig2, ax = plt.subplots()
+        im = ax.imshow(self.tensor_labels_w_doors, cmap=cmap, norm=norm)
+        cbar = ax.figure.colorbar(im)
+
+        return fig, fig2 
 
     def view_plan_shapely(self):
         assert type(self.regions) != None
 
         fig = plt.figure(figsize=SIZE,  dpi=90)
         ax = fig.add_subplot(111)
+        colors = create_colorway(len(self.regions))
 
         for ix, region in enumerate(self.regions):
-            splt.plot_polygon(region.shape, ax=ax, alpha=0.5, color=COLORWAY[ix])
-            ax.annotate(str(ix), region.centroid)
+            splt.plot_polygon(region.shape, ax=ax, alpha=0.5, color=colors[ix])
+            ax.annotate(str(region.label), region.centroid)
 
         return fig 
 
@@ -188,4 +203,5 @@ class FloorPlan2Graph:
         nx.draw_networkx_labels(self.CG, pos)
         self.CG_edge_labels = nx.draw_networkx_edge_labels(self.CG, pos)
         return plt
+    
 
--- helpers.py
+++ helpers.py
@@ -1,4 +1,78 @@
 import re
+from enum import Enum
+
+from shapely import Polygon
+import numpy as np
+import plotly.express as px
+
+
+RGB_CHANNEL_SIZE = 3
+
+DOOR_CODE = 177 # repeated 3 times to make the RGB for doors
+
+COLORWAY = ['#702632', '#A4B494', '#495867',  "#81909E", "#F4442E", "#DB7C26", "#BB9BB0"]
+
+GM = (np.sqrt(5)-1.0)/2.0
+W = 8
+H = W*GM
+SIZE = (W, H)
+
+class Bbox():
+    def __init__(self, arr):
+        assert len(arr) == 4
+        self.min_row=arr[0]
+        self.min_col=arr[1]
+        self.max_row=arr[2]
+        self.max_col=arr[3]
+
+class RegionType(Enum):
+    ROOM = 0
+    DOOR = 1
+
+class Region():
+    bbox:list = None
+    coords:list = None
+    shape:Polygon = None
+    centroid:tuple = None
+    unique_colors:set = None
+    type:RegionType = None
+    label:str = None
+    area:float = None
+
+
+def create_coords(b:Bbox): 
+    # clockwise from origin in bottom-left corner to match example: 
+    # https://shapely.readthedocs.io/en/stable/reference/shapely.LinearRing.html#shapely.LinearRing
+    coords = [
+        # col ==> x, row ==> y 
+        (b.min_col, b.min_row),
+        (b.max_col, b.min_row),
+        (b.max_col, b.max_row),
+        (b.min_col, b.max_row),
+    ]
+    return coords
+
+
+def check_adjacency(curr_region:Region.shape, nb_region:Region.shape):
+    # shapely check for adjacency 
+    # check = curr_region.touches(nb_region)
+    check = curr_region.intersects(nb_region)
+    return int(check) # True = 1, False = 0
+
+def rgb_str_to_rgba(rgb_str):
+    # Extract numeric values from the string using regular expression
+    rgb_values = [int(value) for value in re.findall(r'\d+', rgb_str)]
+    
+    # Convert to RGBA format
+    rgba_values = [value / 255 for value in rgb_values] + [1.0]  # Normalize RGB values to the range [0, 1]
+    
+    return tuple(rgba_values)
+
+def create_colorway(n_colors=20):
+    colors = px.colors.sample_colorscale("turbo", [n/(n_colors -1) for n in range(n_colors)])
+    return [rgb_str_to_rgba(r) for r in colors]
+
+
 
 def print_keys_values(obj):
     if isinstance(obj, dict):
